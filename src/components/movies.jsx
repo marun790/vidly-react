db9028a12e26a4b2876bcd7f9c40cbb24eae126a
Arun <marun790@gmail.com>
2020-01-22 01:09:52 +0530

    completed

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,13 +6,49 @@ class Movies extends Component {
     movies: getMovies()
   };
 
+  deleteMovie(movie) {
+    console.log('Delete', movie);
+    let movies = this.state.movies.filter(mov => mov._id !== movie._id);
+    this.setState({ movies });
+  }
+
   render() {
+    var { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies</p>;
     return (
-      <div>
-        {this.state.movies.map(movie => (
-          <li key={movie._id}>{movie.title}</li>
-        ))}
-      </div>
+      <React.Fragment>
+        <p>There are {count} from database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genere.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.deleteMovie(movie)}
+                    type="button"
+                    className="btn-sm btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
